d6da63ebe5dd323a0d27efabd95a929505284833
Gilbert Parreno <gilbertparreno95@gmail.com>
2020-01-17 11:01:25 +0800

    update commons-sdk to 1.5.0

--- src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
@@ -5,7 +5,15 @@ import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.savings.clients.SavingsApiClient
 
-class NetworkApiFactory(credentials: ApiCredentials, timeout: Long? = null) : BaseApiFactory<SavingsApiClient>(credentials, timeout) {
+class NetworkApiFactory(
+    userAgent: String?,
+    credentials: ApiCredentials,
+    timeout: Long? = null
+) : BaseApiFactory<SavingsApiClient>(
+    userAgent,
+    credentials,
+    timeout
+) {
 
     override fun createClient(baseUrl: String, tokenRefresher: TokenRefresherInterface?): SavingsApiClient {
         createRetrofit(baseUrl, tokenRefresher).apply {
