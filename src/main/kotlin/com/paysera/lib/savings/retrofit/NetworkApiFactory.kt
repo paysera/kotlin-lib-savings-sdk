b62ad070f87430c3794779e5d25185e8aaded599
luksiv <l.sivickas@gmail.com>
2020-01-21 08:59:19 +0200

    Upgrade common sdk

--- src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
@@ -4,15 +4,18 @@ import com.paysera.lib.common.entities.ApiCredentials
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.savings.clients.SavingsApiClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class NetworkApiFactory(
     userAgent: String?,
     credentials: ApiCredentials,
-    timeout: Long? = null
+    timeout: Long? = null,
+    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
 ) : BaseApiFactory<SavingsApiClient>(
     userAgent,
     credentials,
-    timeout
+    timeout,
+    httpLoggingInterceptorLevel
 ) {
 
     override fun createClient(baseUrl: String, tokenRefresher: TokenRefresherInterface?): SavingsApiClient {
