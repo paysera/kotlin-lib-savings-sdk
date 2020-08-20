c66bbaae37574df600cdd3b65b5ae3cb9f670b7a
Edgar Žigis <tranced.freak@gmail.com>
2020-08-20 07:40:49 +0300

    Implement logging interface

--- src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/savings/retrofit/NetworkApiFactory.kt
@@ -1,6 +1,7 @@
 package com.paysera.lib.savings.retrofit
 
 import com.paysera.lib.common.entities.ApiCredentials
+import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.savings.clients.SavingsApiClient
@@ -10,12 +11,14 @@ class NetworkApiFactory(
     userAgent: String?,
     credentials: ApiCredentials,
     timeout: Long? = null,
-    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
+    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC,
+    errorLoggerInterface: ErrorLoggerInterface
 ) : BaseApiFactory<SavingsApiClient>(
     userAgent,
     credentials,
     timeout,
-    httpLoggingInterceptorLevel
+    httpLoggingInterceptorLevel,
+    errorLoggerInterface
 ) {
     override val baseUrl = "https://savings.paysera.com/savings/rest/v1/"
     override val certifiedHosts = listOf("savings.paysera.com")
